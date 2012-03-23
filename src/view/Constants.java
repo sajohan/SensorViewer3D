01ea534684546be609c88a472eaefcbf00de5651
Nyx <simonil89@gmail.com>
2012-03-23 09:24:28 +0100

    Now with icons, and a Constants-class.. might move that to a better place later.

---
+++ src/view/Constants.java
@@ -0,0 +1,24 @@
+package view;
+
+public class Constants {
+	
+	// Image URI's
+	public static final String addSensorIconURI = "./Icons/addsensoriconsmall.png";
+	public static final String handIconURI = "./Icons/handiconsmall.png";
+	public static final String cameraIconURI = "./Icons/cameraiconsmall.png";
+	public static final String selectionIconURI = "./Icons/selecticonsmall.png";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
--- src/view/OptionsPanel.java
+++ src/view/OptionsPanel.java
@@ -7,6 +7,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -16,15 +17,37 @@ import javax.swing.JPopupMenu;
 
 public class OptionsPanel extends JPanel implements ActionListener{
 	
-	JPopupMenu cameraPopupMenu = createCameraMenu();
+	private JPopupMenu cameraPopupMenu = createCameraMenu();
+	private JButton handButton;
+	private JButton sensorButton;
+	private JButton selectionButton;
+	private JButton cameraButton;
+	
+	private ImageIcon handIcon;
+	private ImageIcon addSensorIcon;
+	private ImageIcon selectionIcon;
+	private ImageIcon cameraIcon;
+	
+	
+	
+	
+	
+	
 	public OptionsPanel(){
 		super.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		JButton handButton = createHandButton();
-		JButton sensorButton = createSensorCreationButton();
-		JButton selectionButton = createSelectionButton();
-		JButton cameraButton = createCameraButton();
-
 		
+		handIcon = new ImageIcon(Constants.handIconURI);
+		addSensorIcon = new ImageIcon(Constants.addSensorIconURI);
+		selectionIcon = new ImageIcon(Constants.selectionIconURI);
+		cameraIcon = new ImageIcon(Constants.cameraIconURI);
+		
+		
+		handButton = createHandButton();
+		sensorButton = createSensorCreationButton();
+		selectionButton = createSelectionButton();
+		cameraButton = createCameraButton();
+
+
 		
 		
 		handButton.addActionListener(this);
@@ -50,23 +73,27 @@ public class OptionsPanel extends JPanel implements ActionListener{
 	
 	
 	private JButton createHandButton(){
-		JButton handButton = new JButton("handtool");
+		JButton handButton = new JButton(handIcon);
 		handButton.setSize(new Dimension(100,100));
 		return handButton;
-		}
+	}
+	
+	
+	
 	private JButton createSensorCreationButton(){
-		JButton sensorButton = new JButton("addsensortool");
+		JButton sensorButton = new JButton(addSensorIcon);
 		return sensorButton;
 	
 	
 	}
+	
 	private JButton createSelectionButton(){
-		JButton selectionButton = new JButton("selectiontool");
+		JButton selectionButton = new JButton(selectionIcon);
 		return selectionButton;
 	
 	}
 	private JButton createCameraButton(){
-		JButton cameraButton = new JButton("cameratool");
+		JButton cameraButton = new JButton(cameraIcon);
 		
 		return cameraButton;
 	

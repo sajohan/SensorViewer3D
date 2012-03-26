18e15141a4469aba45d5d468bc24042798655d67
NyxSwede <simonil89@gmail.com>
2012-03-26 09:14:36 +0200

    attempt 2...

---
+++ src/controller/Main.java
@@ -0,0 +1,17 @@
+package controller;
+
+import view.GUI;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+
+                GUI gui = new GUI();
+
+	}
+
+}

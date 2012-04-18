950bc379164b5c696251c271f2f2a7cf00c234ee
Johan Sandström <johan.m.sandstrom@gmail.com>
2012-04-18 09:24:51 +0200

    Added methodstubs to robothandler.

--- src/core/robotarm/RobotHandler.java
+++ src/core/robotarm/RobotHandler.java
@@ -1,5 +1,7 @@
 package core.robotarm;
 
+import javax.vecmath.Point3d;
+
 
 /**
  * Title: Handler for the robotarm
@@ -10,5 +12,19 @@ package core.robotarm;
  *
  */
 public class RobotHandler {
+	/*
+	 * Just some thoughts on how the class could work. Temporary method-parameter types.
+	 */
 
+	
+	/* Reads a group of sensorvalues. Puts it in the sensorvalue datastructure. */
+	public void readSensorGroup(Point3d[] points){
+		
+	}
+	
+	/* Reads a single sensorvalue. Called by readSensorGroup */
+	public void readSingleSensor(Point3d point){
+		
+	}
+	
 }

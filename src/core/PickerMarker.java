428250ae4f85baf4ffed3c45ff16e62fcb3f54d5
Daniel Nicklasson <dixtre@gmail.com>
2012-04-23 11:20:10 +0200

    PickerMarker now appears in red color

--- src/core/PickerMarker.java
+++ src/core/PickerMarker.java
@@ -1,41 +1,64 @@
 package core;
 
+import javax.media.j3d.Appearance;
 import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
+import javax.media.j3d.ColoringAttributes;
+import javax.media.j3d.Material;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
+import javax.vecmath.Color3f;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3f;
 
 import model.SensorValue;
 
 import com.sun.j3d.utils.geometry.Sphere;
-	
-public class PickerMarker extends BranchGroup{
-	
+
+public class PickerMarker extends BranchGroup {
+
 	final static float SPHERE_SIZE = 0.1f;
-	public PickerMarker(){
-		
+
+	public PickerMarker() {
+
 		this.setCapability(ALLOW_DETACH);
 		this.setPickable(false);
-		
+
 	}
 
 	/**
 	 * Draws a sphere where picker pointed
-	 * @param	x	position x of quantity
-	 * @param	y	position y of quantity
-	 * @param	z	position z of quantity
+	 * 
+	 * @param x
+	 *            position x of quantity
+	 * @param y
+	 *            position y of quantity
+	 * @param z
+	 *            position z of quantity
 	 */
-	public void drawSphere(float x, float y, float z){
-		
+	public void drawSphere(float x, float y, float z) {
+
 		Sphere sphere = new Sphere(SPHERE_SIZE);
+
+		Appearance ap = new Appearance();
+		Color3f col = new Color3f(1.0f, 0.0f, 0.0f);
+		ColoringAttributes ca = new ColoringAttributes(col,
+				ColoringAttributes.NICEST);
+
+		Material material = new Material();
+		material.setDiffuseColor(1.0f, 0.0f, 0.0f);
+		material.setShininess(50.0f);
+		ap.setMaterial(material);
+		ap.setColoringAttributes(ca);
+		
+		sphere.setAppearance(ap);
+
 		TransformGroup transformGrp = new TransformGroup();
 		Transform3D transform = new Transform3D();
-		
+
 		transform.setTranslation(new Vector3f(x, y, z));
 		transformGrp.setTransform(transform);
-		
+
 		transformGrp.addChild(sphere);
 		this.addChild(transformGrp);
 	}

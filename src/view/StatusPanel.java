71eb151bc2cf30e1e19908b45fc67dbbc6e4b7f9
Johan Sandström <johan.m.sandstrom@gmail.com>
2012-04-25 14:37:08 +0200

    Made the statuspanel layout better.

--- src/view/StatusPanel.java
+++ src/view/StatusPanel.java
@@ -6,7 +6,11 @@ import static model.Constants.BRIGHT_MIN;
 import static model.Constants.SCALE_INIT;
 import static model.Constants.SCALE_MAX;
 import static model.Constants.SCALE_MIN;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.util.EventListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -29,29 +33,50 @@ public class StatusPanel extends JPanel{
 	
 	public StatusPanel(EventListener eventListener) {
 		
-		// 1 row 4 columns
-		super.setLayout(new GridLayout(1,4));
+		super.setLayout(new GridBagLayout());
 		
 		// Create slider for scaling
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 0.1;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.WEST;
+		
 		JLabel scaleLabel = new JLabel("Scale", JLabel.CENTER);
+		super.add(scaleLabel, c);
+		
+		c.gridx = 1;
+		c.gridy = 0;
 		scaleslider = createSlider(JSlider.HORIZONTAL, SCALE_MIN, SCALE_MAX,
 				SCALE_INIT, Constants.scaleslider);
-
+		super.add(scaleslider, c);
+		
 		// Create slider for brightness
 		JLabel brigthLabel = new JLabel("Brightness", JLabel.CENTER);
+		c.gridx=2;
+		c.gridy=0;
+		super.add(brigthLabel,c);
+		
 		brightSlider = createSlider(JSlider.HORIZONTAL, BRIGHT_MIN, BRIGHT_MAX,
 				BRIGHT_INIT, Constants.brightslider);
+		c.gridx=3;
+		c.gridy=0;
+		super.add(brightSlider,c);
 		
 		scaleslider.addChangeListener((ChangeListener) eventListener);
 		brightSlider.addChangeListener((ChangeListener) eventListener);
 		
+		
+		//Statusmessage
 		status = new JLabel("No object loaded..", JLabel.CENTER);
-
-		super.add(scaleLabel);
-		super.add(scaleslider);
-		super.add(brigthLabel);
-		super.add(brightSlider);
-		super.add(status);
+		status.setHorizontalAlignment(JLabel.RIGHT);
+		c.insets = new Insets(0, 0, 0, 10);
+		c.anchor = GridBagConstraints.EAST;
+		c.weightx = 0.8;
+		c.gridx = 4;
+		c.gridy = 0;
+		super.add(status,c);
 	}
 	
 	public JSlider createSlider(int facing, int min, int max, int init,

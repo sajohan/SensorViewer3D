12fed302e66186e7a5e1cb71295c752265f2f256
NyxSwede <simonil89@gmail.com>
2012-04-25 10:40:05 +0200

    Methods created for external calls for alignment.

--- src/view/GraphicsPane.java
+++ src/view/GraphicsPane.java
@@ -23,11 +23,13 @@ import javax.swing.JPanel;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 
+import model.Point3Dim;
 import model.SensorValue;
 import static model.Constants.*;
 
 import testGroups.CThreePointsMockCloud;
 import testGroups.CThreePointsMockObject;
+import view.graphicscomponents.CThreePObject;
 
 
 /**
@@ -49,6 +51,8 @@ public class GraphicsPane extends JPanel {
 	private BranchGroup group;
 	private final Canvas3D canvas;
 	private OrbitBehavior orbit;
+	private CThreePObject object;
+	private CThreePObject cloud;
 	Vector3d controlVec = new Vector3d(0.0f, -1.0f, 5.0f);
 
 	public GraphicsPane(JFrame frame) {
@@ -56,104 +60,113 @@ public class GraphicsPane extends JPanel {
 		objLoader = new ObjectLoader(null, null);
 
 		GraphicsConfiguration config = SimpleUniverse
-				.getPreferredConfiguration();
+		.getPreferredConfiguration();
 		canvas = new Canvas3D(config);
 
 		canvas.setSize(new Dimension(400, 400));
 
 		univ = new SimpleUniverse(canvas);
-		
+
 		//Set up branchgroup
 		group = new BranchGroup();
 		group.setCapability(BranchGroup.ALLOW_DETACH);
 		group.setCapability(BranchGroup.ALLOW_CHILDREN_EXTEND);
 		group.setCapability(BranchGroup.ALLOW_CHILDREN_READ);
 		group.setCapability(BranchGroup.ALLOW_CHILDREN_WRITE);
-			
-			
-			/**
-			 * Uncomment this to see moveTo() in the works.
-			 */
-			//This is the object to be moved by the moveTo
-			CThreePointsMockCloud CThreePO = new CThreePointsMockCloud(group);
-			//Object containing measurements (i.e. our fluffy cloud) that we move around a bit at first.
-			CThreePointsMockCloud CThreePO2 = new CThreePointsMockCloud(group);
-			
-			
-			/* Crazyvals
-			CThreePO2.setPoint3(new Vector3d(11,8,4));
-			CThreePO2.setPoint1(new Vector3d(2, -11, 5));
-			CThreePO2.setPoint2(new Vector3d(-3, 5, 22));
-			*/
-			group.addChild(CThreePO);
-			group.addChild(CThreePO2);
-
-			/*
-			 * move,rotate and rescale cloud so object can be moved,rotated and scaled to the cloud in testing
-			 */
-			Transform3D mockMove = new Transform3D();
-			Transform3D mockRotate = new Transform3D();
-			Transform3D mockScale = new Transform3D();
-			mockScale.setScale(3);
-//			CThreePO2.setTransform(mockScale);
-			mockMove.setTranslation(new Vector3d(20, 19, 18));
-			mockMove.mul(mockScale);
-			mockRotate.rotX(3);
-			mockMove.mul(mockRotate);
-			mockRotate.rotY(2);
-			mockMove.mul(mockRotate);
-			mockRotate.rotZ(1.111);
-			mockMove.mul(mockRotate);
 
 
-			CThreePO2.setTransform(mockMove);
+		/*
+		 * Create the objects that hold the virtual and physical 
+		 * reference points used in aligninment.
+		 */
+		//This is the object to be moved by the moveTo
+		object = new CThreePObject(group);
+		//Object containing measurement data
+		cloud = new CThreePObject(group);
 
-			
-			CThreePO.moveTo(CThreePO2);
-
-			
+		//		
+		//		/**
+		//		 * Uncomment this to see moveTo() in the works.
+		//		 * DEBUG START
+		//		 */
+		//
+		//
+		//		/* Crazyvals
+		//			CThreePO2.setPoint3(new Vector3d(11,8,4));
+		//			CThreePO2.setPoint1(new Vector3d(2, -11, 5));
+		//			CThreePO2.setPoint2(new Vector3d(-3, 5, 22));
+		//		 */
+		//		group.addChild(CThreePO);
+		//		group.addChild(CThreePO2);
+		//
+		//		/*
+		//		 * move,rotate and rescale cloud so object can be moved,rotated and scaled to the cloud in testing
+		//		 */
+		//		Transform3D mockMove = new Transform3D();
+		//		Transform3D mockRotate = new Transform3D();
+		//		Transform3D mockScale = new Transform3D();
+		//		mockScale.setScale(3);
+		//		//			CThreePO2.setTransform(mockScale);
+		//		mockMove.setTranslation(new Vector3d(20, 19, 18));
+		//		mockMove.mul(mockScale);
+		//		mockRotate.rotX(3);
+		//		mockMove.mul(mockRotate);
+		//		mockRotate.rotY(2);
+		//		mockMove.mul(mockRotate);
+		//		mockRotate.rotZ(1.111);
+		//		mockMove.mul(mockRotate);
+		//
+		//
+		//		CThreePO2.setTransform(mockMove);
+		//
+		//
+		//		CThreePO.moveTo(CThreePO2);
+		//
+		//		/*
+		//		 * DEBUG END
+		//		 */
 
 		setUpLightAndGrid();
-		
-		//move the grid to center of CThreePO 
-//		grid.detach();
-//		CThreePO.addChild(grid);
 
-		
+		//move the grid to center of CThreePO 
+		//		grid.detach();
+		//		CThreePO.addChild(grid);
+
+
 		//create the object that displays the measured quantities in 3d
 		sensorValuesDrawer = new SensorValuesDrawer();
-//		sensorValuesDrawer.drawSphere(0, 0, 0, 1);
+		//		sensorValuesDrawer.drawSphere(0, 0, 0, 1);
 		group.addChild(sensorValuesDrawer);
-		
+
 		//set up view to nominal viewing transform
 		ViewingPlatform vp = univ.getViewingPlatform();
 		vp.setNominalViewingTransform();
-		
+
 		// Set clipdistance
 		canvas.getView().setBackClipDistance(1000);
 		canvas.getView().setFrontClipDistance(0.1);
-		
+
 		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0),
 				10000.0);
-		
+
 		// Setup picking
 		Picker picker = new Picker(canvas, group, bounds);
 		group.addChild(picker);
-		
-		
+
+
 		univ.addBranchGraph(group);
 
 		// Make camera moveable (OrbitBehavior)
 		orbit = new OrbitBehavior(canvas,
 				OrbitBehavior.REVERSE_ALL);
-		
+
 		//enables better zooming
 		orbit.setProportionalZoom(true); 
-		
+
 		//disallows zooming in too close, such as zooming in past origo
 		orbit.setCapability(OrbitBehavior.STOP_ZOOM);
 		orbit.setMinRadius(2.0);
-		
+
 		orbit.setSchedulingBounds(bounds);
 		orbit.setRotationCenter(new Point3d(0,0,0));
 		vp.setViewPlatformBehavior(orbit);
@@ -161,8 +174,29 @@ public class GraphicsPane extends JPanel {
 		frame.add(canvas);
 		frame.pack();
 		frame.setVisible(true);
-		
+
 		enableResize(canvas,500);
+	}
+	
+	/**
+	 * Aligns the object with the cloud using arguments
+	 * @param	 Point3Dim[]	virtualRefPoints	reference points on virtual object
+	 * @param	 Point3Dim[]	physicalRefPoints	reference points on physical object
+	 */
+	public void align(Point3Dim[] virtualRefPoints , Point3Dim[] physicalRefPoints){
+
+		object.setPoint1(new Vector3d(virtualRefPoints[0].x,virtualRefPoints[0].y,virtualRefPoints[0].z));
+		object.setPoint2(new Vector3d(virtualRefPoints[1].x,virtualRefPoints[1].y,virtualRefPoints[1].z));
+		object.setPoint3(new Vector3d(virtualRefPoints[2].x,virtualRefPoints[2].y,virtualRefPoints[2].z));
+
+		cloud.setPoint1(new Vector3d(physicalRefPoints[0].x,physicalRefPoints[0].y,physicalRefPoints[0].z));
+		cloud.setPoint2(new Vector3d(physicalRefPoints[1].x,physicalRefPoints[1].y,physicalRefPoints[1].z));
+		cloud.setPoint3(new Vector3d(physicalRefPoints[2].x,physicalRefPoints[2].y,physicalRefPoints[2].z));
+
+//		group.addChild(object); object is already added to group on setObject()
+		group.addChild(cloud);
+		//perform align (move object to cloud and align )
+		object.moveTo(cloud);
 	}
 
 	/**
@@ -174,37 +208,38 @@ public class GraphicsPane extends JPanel {
 
 		//detatch branchgroup from its parent, replace with new branchgroup
 		group.detach();
+		group.removeChild(object);
 
-		
 		// univ.getLocale().removeBranchGraph(group);
-		group = newModel;
-		group.setCapability(BranchGroup.ALLOW_DETACH);
-		group.setCapability(BranchGroup.ALLOW_CHILDREN_EXTEND);
-		group.setCapability(BranchGroup.ALLOW_CHILDREN_READ);
-		group.setCapability(BranchGroup.ALLOW_CHILDREN_WRITE);
-		
+//		group = newModel;
+//		group.setCapability(BranchGroup.ALLOW_DETACH);
+//		group.setCapability(BranchGroup.ALLOW_CHILDREN_EXTEND);
+//		group.setCapability(BranchGroup.ALLOW_CHILDREN_READ);
+//		group.setCapability(BranchGroup.ALLOW_CHILDREN_WRITE);
+		object.setObject(newModel);
+
 		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0),
 				10000.0);
-		
+
 		//Set picker on the new group
 		Picker picker = new Picker(canvas, group, bounds);
-		group.addChild(picker);
-		
-		lights.detach();
-		grid.detach();
+		group.addChild(object);
 
-		group.addChild(lights);
-		group.addChild(grid);
+//		lights.detach();
+//		grid.detach();
+//
+//		group.addChild(lights);
+//		group.addChild(grid);
 		univ.addBranchGraph(group);
 
-//		univ.getViewingPlatform().setNominalViewingTransform();
-//		
-//        view_tg = univ.getViewingPlatform().getMultiTransformGroup().getTransformGroup(0);
-//        view_tf3d = new Transform3D();
-//        view_tg.getTransform(view_tf3d);
-//		
-//		view_tf3d.lookAt(new Point3d(0d,0d,10d),new Point3d(0d,0d,0d),new Vector3d(0,1,0));
-//        view_tf3d.invert();
+		//		univ.getViewingPlatform().setNominalViewingTransform();
+		//		
+		//        view_tg = univ.getViewingPlatform().getMultiTransformGroup().getTransformGroup(0);
+		//        view_tf3d = new Transform3D();
+		//        view_tg.getTransform(view_tf3d);
+		//		
+		//		view_tf3d.lookAt(new Point3d(0d,0d,10d),new Point3d(0d,0d,0d),new Vector3d(0,1,0));
+		//        view_tf3d.invert();
 		lockOnAxle(CAM_LOCK_X, false);
 	}
 
@@ -229,58 +264,58 @@ public class GraphicsPane extends JPanel {
 		Thread thr1 = new Thread(r1);
 		thr1.start();
 	}
-	
+
 	/**
-	* Sets the view to parallel, that is, things far away looks as close as things up close.
-	*/
+	 * Sets the view to parallel, that is, things far away looks as close as things up close.
+	 */
 	public void setParallelPolicy(){
-	canvas.getView().setProjectionPolicy(View.PARALLEL_PROJECTION);
+		canvas.getView().setProjectionPolicy(View.PARALLEL_PROJECTION);
 	}
 
 	/**
-	* Sets the view to perspective, that is, things far away looks like they're far away.
-	*/
+	 * Sets the view to perspective, that is, things far away looks like they're far away.
+	 */
 	public void setPerspectivePolicy(){
-	canvas.getView().setProjectionPolicy(View.PERSPECTIVE_PROJECTION);
+		canvas.getView().setProjectionPolicy(View.PERSPECTIVE_PROJECTION);
 	}
-	
+
 	/**
 	 * Places camera on the axle provided as argument.
 	 * @param int	axle	from Model.Constants the axle(x,y or z) to place the camera
 	 */
 	public void lockOnAxle(int axle, boolean reversed) {
 		//get transformgroup number one (the camera), put it in view_tf3d variable
-        view_tg = univ.getViewingPlatform().getMultiTransformGroup().getTransformGroup(0);
-        view_tf3d = new Transform3D();
-        view_tg.getTransform(view_tf3d);
-        //get constant camera distance from origo,
-        //reverse if argument "reversed" demands so
-        double camDistance = CAM_DISTANCE;
-        if(reversed)
-        	camDistance = (-camDistance);
-        
-        //using the camera's transformgroup, place the camera at CAM_DISTANCE
-        //from origo on the axis provided in the mathod argument
-        //note to self: argument 1 to lookAt() (eye position) 
-        //may never include z = 0 when placing camera on Y-axis
-        switch(axle){
-        case CAM_LOCK_X: view_tf3d.lookAt(new Point3d(camDistance,0d,0d),new Point3d(0d,0d,0d),new Vector3d(0,1,0)); break;
-        case CAM_LOCK_Y: view_tf3d.lookAt(new Point3d(0d,camDistance,0.1d),new Point3d(0d,0d,0d),new Vector3d(0,1,0)); break;
-        case CAM_LOCK_Z: view_tf3d.lookAt(new Point3d(0d,0d,camDistance),new Point3d(0d,0d,0d),new Vector3d(0,1,0)); break;
-        }
-		
-        //Note: Transform3D.lookAt() requires .invert() call after each use
-        view_tf3d.invert(); 
-        view_tg.setTransform(view_tf3d); //Perform the camera move
-        setParallelPolicy();
-        System.out.println(canvas.getView().getFieldOfView());
-        canvas.getView().setFieldOfView(0.29);
-//        try {
-//			wait(599);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-        System.out.println("after"+canvas.getView().getFieldOfView());
+		view_tg = univ.getViewingPlatform().getMultiTransformGroup().getTransformGroup(0);
+		view_tf3d = new Transform3D();
+		view_tg.getTransform(view_tf3d);
+		//get constant camera distance from origo,
+		//reverse if argument "reversed" demands so
+		double camDistance = CAM_DISTANCE;
+		if(reversed)
+			camDistance = (-camDistance);
+
+		//using the camera's transformgroup, place the camera at CAM_DISTANCE
+		//from origo on the axis provided in the mathod argument
+		//note to self: argument 1 to lookAt() (eye position) 
+		//may never include z = 0 when placing camera on Y-axis
+		switch(axle){
+		case CAM_LOCK_X: view_tf3d.lookAt(new Point3d(camDistance,0d,0d),new Point3d(0d,0d,0d),new Vector3d(0,1,0)); break;
+		case CAM_LOCK_Y: view_tf3d.lookAt(new Point3d(0d,camDistance,0.1d),new Point3d(0d,0d,0d),new Vector3d(0,1,0)); break;
+		case CAM_LOCK_Z: view_tf3d.lookAt(new Point3d(0d,0d,camDistance),new Point3d(0d,0d,0d),new Vector3d(0,1,0)); break;
+		}
+
+		//Note: Transform3D.lookAt() requires .invert() call after each use
+		view_tf3d.invert(); 
+		view_tg.setTransform(view_tf3d); //Perform the camera move
+		setParallelPolicy();
+		System.out.println(canvas.getView().getFieldOfView());
+		canvas.getView().setFieldOfView(0.29);
+		//        try {
+		//			wait(599);
+		//		} catch (InterruptedException e) {
+		//			e.printStackTrace();
+		//		}
+		System.out.println("after"+canvas.getView().getFieldOfView());
 	}
 	/**
 	 * Sets up the light and x,y,z origo axes in the universe.
@@ -304,7 +339,7 @@ public class GraphicsPane extends JPanel {
 		sensorValuesDrawer.drawSensorValue(s);
 		univ.addBranchGraph(group);
 	}
-	
+
 	public Lighting getLights() {
 		return lights;
 	}
@@ -312,7 +347,7 @@ public class GraphicsPane extends JPanel {
 	public void setLights(Lighting lights) {
 		this.lights = lights;
 	}
-	
+
 	public Grid getGrid() {
 		return grid;
 	}
